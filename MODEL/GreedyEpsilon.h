7f347e09ea651fd06370a3bf1f1ab975dc72963d
MoonFlowww <161968556+MoonFlowww@users.noreply.github.com>
2024-10-17 11:48:52 +0200

    Create GreedyEpsilon.h

---
+++ MODEL/GreedyEpsilon.h
@@ -0,0 +1,25 @@
+#include <random>
+
+//RL policy
+class EpsilonGreedyPolicy {
+public:
+    double epsilon;
+    std::mt19937 gen;
+    std::uniform_real_distribution<> dis;
+
+    EpsilonGreedyPolicy(double eps) : epsilon(eps), gen(std::random_device{}()), dis(0.0, 1.0) {}
+
+    int select_action(const std::vector<double>& action_values) {
+        if (dis(gen) < epsilon) {
+            // Exploration: random action
+            std::uniform_int_distribution<> action_dist(0, action_values.size() - 1);
+            return action_dist(gen);
+        } else {
+            // Exploitation: highest value
+            return std::distance(action_values.begin(), std::max_element(action_values.begin(), action_values.end()));
+        }
+    }
+    void decay_epsilon(double decay_rate) {
+        epsilon *= decay_rate;
+    }
+};
